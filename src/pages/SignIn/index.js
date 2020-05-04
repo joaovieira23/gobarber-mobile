11fcee383997dd7a81ffd71fda55dabe0af7b438
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-04 17:12:26 -0300

    feat: create input and button components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,22 @@
 import React from 'react';
-
 import { Text } from 'react-native';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 import Background from '../../components/Background';
 
 function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+
+      <Input
+        style={{ marginTop: 30 }}
+        icon="call"
+        placeholder="Digite sua senha"
+      />
+
+      <Button>Entrar</Button>
     </Background>
   );
 }
