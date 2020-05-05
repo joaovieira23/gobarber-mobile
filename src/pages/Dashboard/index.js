ac03c280df922b1e156ebae55baa789ca1c6c383
joaovieira23 <joaovictorvieira.23@hotmail.com>
2020-05-05 15:50:59 -0300

    feat: TabBar styles

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+
+import { Icon } from 'react-native-vector-icons/MaterialIcons';
+
+import Background from '../../components/Background';
 
 // import { Container } from './styles';
 
-const Dashboard = () => {
-  return <View />;
-};
+export default function Dashboard() {
+  return <Background />;
+}
 
-export default Dashboard;
+Dashboard.navigationOptions = {
+  tabBarLabel: 'Agendamentos',
+  tabBarIcon: ({ tintColor }) => (
+    <Icon name="event" size={20} color={tintColor} />
+  ),
+};
